8e49d90f2252d3d0b33543e662517df975b72e62
Константинов Дмитрий <poethrenoff@gmail.com>
2017-01-15 17:49:50 +0300

    Странные изменения

--- htdocs/script/stihi.ru/random.php
+++ htdocs/script/stihi.ru/random.php
@@ -4,7 +4,7 @@
  */
 include_once dirname(dirname(dirname(__FILE__))) . '/config/config.php';
 
-$server = 'http://www.stihi.ru';
+$server = 'https://www.stihi.ru';
 $login_url = '/cgi-bin/login/intro.pl';
 $list_url = '/login/page.html?list';
 $put_url = '/login/page.html?put';
@@ -13,8 +13,7 @@ $user_login = 'poethrenoff';
 $user_password = '**********';
 $user_agent = 'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/534.24 (KHTML, like Gecko) Chrome/11.0.696.14 Safari/534.24';
 
-//    $server_rubric = '18'; // Всякое, прочее и остальное
-$server_rubric = '19'; // Практика
+$server_rubric = '18'; // Всякое, прочее и остальное
 
 $ch = curl_init();
 
@@ -39,7 +38,7 @@ curl_setopt($ch, CURLOPT_POST, 0 );
 curl_setopt($ch, CURLOPT_URL, $server . $list_url . "&book={$server_rubric}" );
 $result = curl_exec($ch);
 
-preg_match_all('/\<a href="\/(\d+\/\d+\/\d+\/\d+)"\>(.+)\<\/a\>/isU', $result, $matches, PREG_SET_ORDER);
+preg_match_all('/\<a href="http\:\/\/www\.stihi\.ru\/(\d+\/\d+\/\d+\/\d+)"\>(.+)\<\/a\>/isU', $result, $matches, PREG_SET_ORDER);
 
 // Перемешиваем его
 shuffle($matches);
