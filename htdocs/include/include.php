b31f0f998d645de24cd4c08272acf5d31f4db8cd
Dmitry Konstantinov <poethrenoff@gmail.com>
2024-02-01 09:25:25 +0300

    fix(tool): path to vendors

--- htdocs/include/include.php
+++ htdocs/include/include.php
@@ -4,7 +4,7 @@ include_once 'helpers.php';
 
 // Подключение автозагрузчиков
 include_once 'autoload.php';
-include_once 'vendor/autoload.php';
+include_once dirname(__FILE__) . '/../vendor/autoload.php';
 
 // Подключение пользовательских исключений
 include_once 'exception.php';
