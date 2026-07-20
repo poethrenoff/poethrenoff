b30c032a8493a01068ead04c8582939a30d57a44
Dmitry Konstantinov <d.konstantinov@aitarget.com>
2026-07-20 14:56:29 +0300

    feat(templates): add per-site entry points and htdocs multi-domain web roots

---
+++ htdocs/www/index.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Kernel;
+
+$_SERVER['APP_SITE_CONTEXT'] = 'www';
+
+require_once dirname(__DIR__, 2).'/vendor/autoload_runtime.php';
+
+return static function (array $context) {
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+};
