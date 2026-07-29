a6111b4e22b3afb962153e74d79fdefe776e3d2a
Dmitry Konstantinov <d.konstantinov@aitarget.com>
2026-07-29 07:12:28 +0300

    chore(migrations): make work_vote.user_agent_hash NOT NULL

---
+++ migrations/Version20260729040804.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260729040804 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE work_vote CHANGE user_agent_hash user_agent_hash VARCHAR(64) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE work_vote CHANGE user_agent_hash user_agent_hash VARCHAR(64) DEFAULT \'\' NOT NULL');
+    }
+}
